93eea0e641b806a3d3d5bf079a20bdc3af335a7d
Zhang Yiwen <yiwen@pigeonlab.com>
2015-12-23 13:29:20 +0800

    in progress

--- Crossword.js
+++ Crossword.js
@@ -1,7 +1,11 @@
+"use strict";
+
 var _ = require('underscore');
 const EMPTY_SPACE = 'EMPTY_SPACE', 
       HORIZONTAL_SPACE = 'HORIZONTAL_SPACE', 
       VERTICAL_SPACE = 'VERTICAL_SPACE';
+
+
 
 
 class Crossword {
@@ -10,14 +14,23 @@ class Crossword {
 		this.size = size;
 		this.horizontalEntries = [];
 		this.verticalEntries = [];
+		this.map = new ReferenceMap(size);
 	}
 
 	setHorizontalEntries(entryList){
 		this.horizontalEntries = entryList;
+		let map = this.map;
+		_.each(entryList, function(entry, entryIndex){
+			map.addEntry(entry, entryIndex);
+		})
 	}
 
-	setVertialEntries(entryList){
+	setVerticalEntries(entryList){
 		this.verticalEntries = entryList;
+		let map = this.map;
+		_.each(entryList, function(entry, entryIndex){
+			map.addEntry(entry, entryIndex);
+		})
 	}
 
 	getHorizontalEntries(){
@@ -30,9 +43,21 @@ class Crossword {
 
 
 	printCrossword(){
-		let map = new ReferenceMap(size);
-		
 
+		for (var i = 0; i <= this.size; i++){
+			let rowString = '';
+			for(var j = 0; j <= this.size; j++){
+				let spaceRef = this.map.getReference(i, j);
+				let space = spaceRef.getSpace(this.getHorizontalEntries(), this.getVerticalEntries());
+				let content = "*"
+				if(!_.isNull(space)){
+					content = space.getAnswer();
+				}
+				rowString = rowString + content + ' ';
+				
+			}
+			console.log(rowString);
+		}
 	}
 
 }
@@ -43,13 +68,13 @@ class ReferenceMap {
 	constructor(size){
 		this.size = size;
 		this.map = [];
-		initialise();	
+		this.initialise();	
 	}
 
 	initialise(){
-		for (var i  = 0; i < this.size; i ++){
+		for (var i  = 0; i <= this.size; i ++){
 			let row = [];
-			for (var j = 0; j < this.size; j ++){
+			for (var j = 0; j <= this.size; j ++){
 				row.push(new SpaceReference(EMPTY_SPACE, -1, -1));
 			}
 			this.map.push(row);
@@ -57,18 +82,21 @@ class ReferenceMap {
 	}
 
 	getReference(x, y){
-		this.map[x][y];
+		return this.map[x][y];
 	}
 
 	addEntry(entry, entryIndex){
 		var entryType = entry.getType();
-		_.each(entry, function(space, spaceIndex){
+		let self = this;
+		_.each(entry.getSpaces(), function(space, spaceIndex){
+
 			let position = space.getPosition();
-			let spaceReference = this.getReference(position[0], position[1]);
+			let spaceReference = self.getReference(position[0], position[1]);
 			spaceReference.setIndexes(entryIndex, spaceIndex);
 			spaceReference.setType(entryType);
 		})
 	}
+
 }
 
 class SpaceReference {
@@ -79,7 +107,7 @@ class SpaceReference {
 		this.spaceIndex = spaceIndex;
 	}
 
-	setIndexes(entryInex, spaceIndex){
+	setIndexes(entryIndex, spaceIndex){
 		this.entryIndex = entryIndex;
 		this.spaceIndex = spaceIndex;
 	}
@@ -88,15 +116,16 @@ class SpaceReference {
 		this.type = type;
 	}
 
-	getSpaceAnswer(HorizontalEntries, VerticalEntries){
+	getSpace(horizontalEntries, verticalEntries){
+
 		let spaceIndex = this.spaceIndex;
 		let entryIndex = this.entryIndex;
-		if(type == HORIZONTAL_SPACE){
-			return HorizontalEntries[entryIndex][spaceIndex].getAnswer();
-		} else if (type == VERTICAL_SPACE){
-			return VerticalEntries[entryIndex][spaceIndex].getAnswer();
+		if(this.type == HORIZONTAL_SPACE){
+			return horizontalEntries[entryIndex].getSpace(spaceIndex);
+		} else if (this.type == VERTICAL_SPACE){
+			return verticalEntries[entryIndex].getSpace(spaceIndex);
 		} else {
-			return '*'
+			return null;
 		}
 	}
 
@@ -126,6 +155,10 @@ class Entry {
 		return this.spaces;
 	}
 
+	getSpace(index){
+		return this.spaces[index];
+	}
+
 	isCorrect(){
 		let spaces = this.getSpaces();
 		if(spaces.length > 0){
@@ -142,8 +175,8 @@ class Space {
 	constructor(answer, position){
 		this.answer = answer;
 		this.value = '';
-		this.x = x;
-		this.y = y;
+		this.x = position[0];
+		this.y = position[1];
 	}
 
 	isCorrect(){
@@ -159,7 +192,7 @@ class Space {
 	}
 
 	getPosition(){
-		return (this.x, this.y);
+		return [this.x, this.y];
 	}
 
 	setValue(value){
@@ -167,3 +200,20 @@ class Space {
 	}
 
 }
+
+var crossword = new Crossword(2);
+
+var aa = new Space('A', [0, 0]);
+var ab = new Space('B', [0, 1]);
+var bb = new Space('C', [1, 1]);
+var cb = new Space('D', [2, 1]);
+
+var entryOne = new Entry(2, HORIZONTAL_SPACE);
+var entryTwo = new Entry(2, VERTICAL_SPACE);
+entryOne.setSpaces([aa, ab]);
+entryTwo.setSpaces([bb, cb]);
+
+crossword.setVerticalEntries([entryTwo]);
+crossword.setHorizontalEntries([entryOne]);
+
+crossword.printCrossword();
